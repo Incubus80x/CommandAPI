a8a199084c867e0ec0863c24a0b67247931315f0
Dionysus <dglonti@gmail.com>
2020-10-08 22:37:37 +0400

    DI implementation in controller

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -7,10 +9,32 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo __repository;
+
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            __repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = __repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = __repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(commandItem);
+            }
         }
     }
 }
